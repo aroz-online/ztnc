d61e1eed9ca1297cd1daef9618664778f312326d
Toby Chui <tobychui@users.noreply.github.com>
2025-11-21 20:33:45 +0800

    Fixed update introspect script bug

--- update_introspect.sh
+++ update_introspect.sh
@@ -2,5 +2,5 @@
 make
 
 # Uncomment the one with suitable build platform
-# ./build/ztnc_linux_amd64 > .introspect
-./build/ztnc_windows_amd64.exe > .introspect
+# ./build/ztnc_linux_amd64 -introspect > .introspect
+./build/ztnc_windows_amd64.exe -introspect > .introspect
